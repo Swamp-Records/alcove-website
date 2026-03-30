ca01048f76ebc1823e73a9ba843837e4ccf5d4ea
jjennasin <jennasin@ufl.edu>
2026-03-30 15:48:05 -0400

    calendly is yellow

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -4,22 +4,30 @@ export const metadata = {
   description: "Book this local indie band for your next event.",
 };
 
+
 export default function Booking() {
   return (
-    <main className="min-h-screen px-6 py-16">
+    <main className="min-h-screen bg-[#F9F4D2] px-6 py-16">
       <div className="mx-auto flex max-w-6xl flex-col items-center">
-        <h1 className="mb-8 text-center font-crostan text-4xl text-white">
+        <h1 className="mb-8 text-center font-crostan text-4xl text-black">
           Book Alcove
         </h1>
 
-        <div className="w-full overflow-hidden rounded-2xl bg-white/90 shadow-lg">
+        <div className="w-full overflow-hidden rounded-2xl bg-[#F9F4D2] shadow-lg">
+          {/*
           <iframe
             src="https://calendly.com/jennasin1120/30min"
             width="100%"
             height="700"
-            frameBorder="0"
-            title="Calendly Booking"
-          ></iframe>
+          />
+            */}
+          <iframe
+            src="https://calendly.com/jennasin1120/30min?background_color=F9F4D2&text_color=000000&primary_color=082a5b"
+            width="100%"
+            height="700"
+            className="border-none"
+          />
+
         </div>
       </div>
     </main>
