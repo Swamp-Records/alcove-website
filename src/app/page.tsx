ad9ce796a18108cabe51bb86e1462bbf8416875e
jjennasin <jennasin@ufl.edu>
2026-04-06 14:49:10 -0400

    deleted jenna's version of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,9 @@ import PostcardSection from "./_components/PostcardSection";
 
 export default function Home() {
   return (
-    // <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    //   <h1>This is the <em>home</em> page</h1>
-    // </div>
-    <main>
-            <PostcardSection />
-    </main>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>This is the <em>home</em> page</h1>
+    </div>
+  
   );
 }
